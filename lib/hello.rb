3eb0db6427f8079273d73367c0c9af4573e66fc6
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-07 17:28:09 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,10 @@
 def hello_t(array)
-  array.each do 
+  array.each do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
+    end
+  end
 end
 
 # call your method here!
-
+hello_t(array)
